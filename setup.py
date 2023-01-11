df6059a0369cbce34ebb1cc16cb10bc3fe4cd439
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-01-11 13:32:00 +0800

    alvistack/0.19.0
        git clean -xdf
        tar zcvf ../python-antsibull-changelog_0.19.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-antsibull-changelog.spec ../python-antsibull-changelog_0.19.0-1.spec
        mv ../python*-antsibull-changelog*0.19.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/ansible-community-antsibull-changelog-0.19.0/
        rm -rf ../python*-antsibull-changelog*0.19.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['antsibull_changelog']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML',
+ 'docutils',
+ 'packaging',
+ 'rstcheck>=3.0.0,<7.0.0',
+ 'semantic_version']
+
+entry_points = \
+{'console_scripts': ['antsibull-changelog = antsibull_changelog.cli:main']}
+
+setup_kwargs = {
+    'name': 'antsibull-changelog',
+    'version': '0.19.0',
+    'description': 'Changelog tool for Ansible-base and Ansible collections',
+    'author': 'Felix Fontein',
+    'author_email': 'felix@fontein.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/ansible-community/antsibull-changelog',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
