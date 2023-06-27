564ec7c09e32b64eb7896583092dfe0abd1ca4e0
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-06-27 20:59:22 +0800

    alvistack/0.20.0
        git clean -xdf
        tar zcvf ../python-antsibull-changelog_0.20.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-antsibull-changelog.spec ../python-antsibull-changelog_0.20.0-1.spec
        mv ../python*-antsibull-changelog*0.20.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/ansible-community-antsibull-changelog-0.20.0/
        rm -rf ../python*-antsibull-changelog*0.20.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='antsibull-changelog',
+    version='0.20.0',
+    description='Changelog tool for Ansible-base and Ansible collections',
+    author_email='Felix Fontein <felix@fontein.de>, Toshio Kuratomi <a.badger@gmail.com>, Matt Clay <matt@mystile.com>',
+    maintainer_email='Felix Fontein <felix@fontein.de>, Maxwell G <maxwell@gtmx.me>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Ansible',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Typing :: Typed',
+    ],
+    install_requires=[
+        'docutils',
+        'packaging',
+        'pyyaml',
+        'rstcheck<7.0.0,>=3.0.0',
+        'semantic-version',
+    ],
+    extras_require={
+        'codeqa': [
+            'flake8>=3.8.0',
+            'pylint',
+            'reuse',
+        ],
+        'coverage': [
+            'coverage[toml]',
+        ],
+        'dev': [
+            'antsibull-changelog[codeqa]',
+            'antsibull-changelog[coverage]',
+            'antsibull-changelog[test]',
+            'antsibull-changelog[typing]',
+            'nox',
+        ],
+        'test': [
+            'mock',
+            'pytest',
+            'pytest-cov',
+            'pytest-error-for-skips',
+        ],
+        'typing': [
+            'mypy',
+            'pyre-check~=0.9.17',
+            'types-docutils',
+            'types-pyyaml',
+            'types-toml',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'antsibull-changelog = antsibull_changelog.cli:main',
+        ],
+    },
+    packages=[
+        'antsibull_changelog',
+    ],
+    package_dir={'': 'src'},
+)
